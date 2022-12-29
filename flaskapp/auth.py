341c49589134ec19b17f1f282e6f14bc82a66bd3
Jakob Nacanaynay <63821647+j-nac@users.noreply.github.com>
2022-12-28 22:39:40 -0800

    Add user registration

--- flaskapp/auth.py
+++ flaskapp/auth.py
@@ -1,11 +1,14 @@
 from flaskapp import db
 from flaskapp.models import User
+from datetime import datetime
 
 def authenticate(username, password):
-    user = db.first_or_404(db.select(User).filter_by(email=username))
+    user = db.one_or_404(db.select(User).filter_by(email=username))
     if user.check_password(password.encode('utf-8')):
+        user.last_login_timestamp = datetime.now()
+        db.session.commit()
         return user
 
 def identity(payload):
     user_id = payload['identity']
-    return db.get_or_404(User, user_id)
+    return db.one_or_404(User, user_id)
--- flaskapp/resources.py
+++ flaskapp/resources.py
@@ -1,8 +1,26 @@
-from flask_restful import Resource
-from flaskapp import api
+from flask_restful import Resource, reqparse
+from flaskapp import api, db, bcrypt
+from flaskapp.models import User
 
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
+# Need to add more input validation for security
+class Register(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('username', required=True)
+        parser.add_argument('password', required=True)
+        parser.add_argument('email', required=True)
+        parser.add_argument('profile_picture')
+        parser.add_argument('description')
+        args = parser.parse_args()
+
+        hashed_password = bcrypt.generate_password_hash(args['password']).decode('utf-8')
+        user = User(username=args['username'], password=hashed_password, email=args['email'], profile_picture=args['profile_picture'], description=args['description'])
+        db.session.add(user)
+        db.session.commit()
+
 api.add_resource(HelloWorld, '/')
+api.add_resource(Register, '/register')
